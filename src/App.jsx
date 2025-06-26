3c732aafa1a55632d7ec269994e4ba9ffff6ce5d
Lexi Solomonic <lsolomonic125@gmail.com>
2025-06-26 16:54:03 -0400

    placeholder logo + link

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 
 function App() {
   return (
-    <div>
+    <div className="bg-black">
+      <h1 className="text-white text-[50px]">
+        <a href="https://github.com/lsolomonic/COP4331-8-LargeProject">
+        <img src='/lilguy.png' className="w-15 h-10 float-left mt-4"/>
+        </a>
+        Project Title
+        </h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
