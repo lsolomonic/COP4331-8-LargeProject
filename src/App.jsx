3dee035d689f66581fae0d9cf05a934bf26ce498
Lexi Solomonic <lsolomonic125@gmail.com>
2025-06-26 13:55:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './Login'
+
+function App() {
+  return (
+    <>
+        <h1 className="text-[50px] text-sky-500 ml-20 ">Project Title</h1>
+        <Login /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import './App.css'
+
+function Login() {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        email: '',
+        verifyemail: ''
+    });
+
+    const [logState, setLogState] = useState('Please Log In:')
+
+    const [visState, setVisState] = useState('hidden');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevFormData => ({
+        ...prevFormData,
+        [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //check state
+        if (logState == "Please Log In:") {
+        //send to login API
+        } else {
+        //send to register API 
+        }
+
+    }
+
+    const handleLog = (e) => {
+        if (logState == "Please Log In:"){
+        setLogState('Please Register:');
+        setVisState('visible');
+        } else {
+        setLogState('Please Log In:');
+        setVisState('hidden');
+        }
+    }
+
+    return (
+        <>
+            <div id="logState" className="text-[50px] text-center text-white">{logState}</div>
+            <form onSubmit={handleSubmit}>
+                <div id="loginDiv" className="text-[40px] text-center bg-gray-400 rounded-xl text-white ">  
+                <input type="text" name="username" value={formData.username} onChange={handleChange} className="border-2 border-black rounded-2xl text-center mt-5" placeholder="Username" required></input> <br /> 
+                <input type="password" name="password" value={formData.password} onChange={handleChange} className="border-2 border-black rounded-2xl text-center mt-5" placeholder="Password" required></input> <br />
+                <div id="regDiv" className = {visState}>
+                    <input type="text" name="email" value={formData.email} onChange={handleChange} className="border-2 border-black rounded-2xl text-center mt-5" placeholder="Email"></input> <br />
+                    <input type="text" name="verifyemail" value={formData.verifyemail} onChange={handleChange}className="border-2 border-black rounded-2xl text-center mt-5" placeholder="Verify Email"></input>
+                </div> 
+                <button type="submit" className="border-black bg-sky-500 hover:bg-sky-700 border-2 rounded-3xl w-100 text-center mt-5">Continue</button>
+                </div>
+            </form>
+            <div className="text-[40px] text-center text-white">No account yet? <a className="text-sky-500 hover:text-sky-700" onClick={handleLog}>Register here.</a></div>
+        </>
+    )
+}
+
+export default Login; 
